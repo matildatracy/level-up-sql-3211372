d3f830241e3e679ea0bde5525114d6ab1426bf42
matildatracy <124531531+matildatracy@users.noreply.github.com>
2024-07-30 23:28:45 +0000

    2.5

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,12 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+update loans 
+set returneddate = '2022-07-05'
+where bookid in (select books.bookid from books where barcode in (6435968624, 5677520613, 8730298424))
+and returneddate is null
+
+select *
+from loans 
+where bookid in (select books.bookid from books where barcode in (6435968624, 5677520613, 8730298424))
+order by returneddate desc
