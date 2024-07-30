336816eaa5f0aa8808a2e091b5b04bf133002b67
matildatracy <124531531+matildatracy@users.noreply.github.com>
2024-07-30 23:17:10 +0000

    2.3

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,17 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+select firstname, lastname, patronid
+from patrons 
+where firstname = 'Jack' and lastname = 'Vaan' and email = 'jvaan@wisdompets.com'
+
+select title, bookid
+from books 
+where barcode in (2855934983, 4043822646) 
+
+insert into loans (bookid, patronid, loandate, duedate)
+values (11, 50, '2022-08-25', '2022-09-08'),
+(93, 50, '2022-08-25', '2022-09-08')
+
+select * from loans 
+order by loanid desc
