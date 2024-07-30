b6baea001b04425d4e36e4eadd611cd399840a1d
matildatracy <124531531+matildatracy@users.noreply.github.com>
2024-07-30 02:11:13 +0000

    1.11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,14 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+SELECT * FROM Customers 
+WHERE Customers.FirstName = 'Cleo' AND Customers.LastName = 'Goldwater';
+
+SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad';
+
+UPDATE Customers 
+SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad')
+WHERE Customers.FirstName = 'Cleo' AND Customers.LastName = 'Goldwater';
+
+SELECT * FROM Customers 
+WHERE Customers.FirstName = 'Cleo' AND Customers.LastName = 'Goldwater';
