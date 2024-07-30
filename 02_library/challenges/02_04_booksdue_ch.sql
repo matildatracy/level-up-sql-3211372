88ef3f6254deaaedef0e7f85fea373ba24fbb82e
matildatracy <124531531+matildatracy@users.noreply.github.com>
2024-07-30 23:21:50 +0000

    2.4

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,11 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+select duedate, title, firstname, email
+from loans l
+join books b 
+on l.bookid = b.bookid 
+join patrons p 
+on l.patronid = p.patronid
+where duedate = '2022-07-13'
+and returneddate is null
