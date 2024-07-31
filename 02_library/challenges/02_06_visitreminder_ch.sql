785f37cdbe33a03a5f5727338086533ad2bf74e1
matildatracy <124531531+matildatracy@users.noreply.github.com>
2024-07-31 03:15:21 +0000

    2.6

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,8 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+select firstname, lastname, email, p.patronid, count(loanid) as Num_Books_Checked_Out
+from loans l
+join patrons p on l.patronid = p.patronid 
+group by p.patronid 
+order by Num_Books_Checked_Out
+limit 10
