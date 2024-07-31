3b8af29b16e801ea9f47f0646bd0486304b59c92
matildatracy <124531531+matildatracy@users.noreply.github.com>
2024-07-31 03:31:35 +0000

    2.8

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -3,5 +3,16 @@
 -- Report 1: Show how many titles were published 
 -- in each year.
 
+select published, count(DISTINCT title) as pub_count 
+from books 
+group by published
+order by pub_count desc
+
 -- Report 2: Show the five books that have been
 -- checked out the most.
+select title, count(loanid) as check_count 
+from loans 
+join books on loans.bookid = books.bookid 
+group by title 
+order by check_count desc
+limit 5
